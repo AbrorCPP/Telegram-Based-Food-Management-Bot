ade3885cc6783c728ed34440c006876cb4b1d295
AbrorCPP <abrorboltayev4843@gmail.com>
2026-02-23 14:54:04 +0500

    first commit i suppose

--- utils/db_api/db.py
+++ utils/db_api/db.py
@@ -37,17 +37,4 @@ class Database:
 
         return data
 
-    def create_users_table(self) -> None:
-        """
-        Creates users table
-        :return: None
-        """
-        sql = """
-            CREATE TABLE IF NOT EXISTS users(
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                telegram_id INT NOT NULL UNIQUE,
-                fullname VARCHAR(100),
-                username VARCHAR(100)
-            )
-        """
-        self.execute(sql)
+
